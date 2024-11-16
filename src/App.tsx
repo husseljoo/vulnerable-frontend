2fc72420f18df74a7578a36214d0aa6024ca1dfc
Ubuntu <fred@azure-vm.mwhcog1xnh3exc5fcvkj1j03ia.ax.internal.cloudapp.net>
2024-11-16 19:15:08 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <header>
+      <h1> Husseljo Website</h1>
+      <nav>
+          <ul>
+              <li> <a href="#"> Register </a></li>
+              <li> <a href="#"> Login </a></li>
+          </ul>
+      </nav>
+      </header>
+  )
+}
+
+export default App
